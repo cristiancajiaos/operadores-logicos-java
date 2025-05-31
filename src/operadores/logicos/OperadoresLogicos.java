0d13aec77f3a4e28fa7ea76de0710c4acb57e04c
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-05-31 12:20:32 -0400

    Agregados operadores lógicos

--- src/operadores/logicos/OperadoresLogicos.java
+++ src/operadores/logicos/OperadoresLogicos.java
@@ -1,20 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package operadores.logicos;
 
-/**
- *
- * @author crist
- */
 public class OperadoresLogicos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Primera variable
+        boolean valor1 = true;
+        // Segunda variable
+        boolean valor2 = true;
+        // Tercera variable
+        boolean valor3 = true;
+        // Cuarta variable
+        boolean valor4 = false;
+        
+        // Operador lógico: &&
+        System.out.println("Primer resultado: " + (valor1 && valor2)); // true
+        System.out.println("Segundo resultado: " + (valor3 && valor4)); // false
+        
+        // Operador lógico: ||
+        System.out.println("Tercer resultado: " + (valor1 || valor2)); // true
+        System.out.println("Cuarto resultado: " + (valor1 || valor4)); // true
+        
+        // Operador lógico: !
+        System.out.println("Quinto resultado: " + (!valor1)); // false
+        System.out.println("Sexto resultado: " + (!valor4)); // true
     }
     
 }
